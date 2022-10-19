bff8be92f7eb7d683721765fa1a48ec642f25f5b
Muhamad Umer <umerdev228@gmail.com>
2022-10-19 21:16:39 +0500

    fixes

--- routes/api.php
+++ routes/api.php
@@ -15,6 +15,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-With');
+header('Access-Control-Allow-Credentials: true');
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
